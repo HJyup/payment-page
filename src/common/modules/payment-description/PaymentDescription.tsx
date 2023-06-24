1243263549debbd5d287b566ffc78e7952e85bfb
d_butov <danyil.butov@gmail.com>
2023-06-24 18:37:38 +0100

    fix: fixed `'` symbol

--- src/common/modules/payment-description/PaymentDescription.tsx
+++ src/common/modules/payment-description/PaymentDescription.tsx
@@ -2,10 +2,9 @@ import styles from "./PaymentDescription.module.scss";
 const PaymentDescription = () => (
   <div className={styles.container}>
     <p className={styles.text}>
-      {/* eslint-disable-next-line react/no-unescaped-entities */}
-      You'll have your <b>Plan Pro during 1 year</b>. After this period of time,
-      your plan will be <b>automatically renewed</b> with its original price
-      without any discounts applied.
+      You&apos;ll have your <b>Plan Pro during 1 year</b>. After this period of
+      time, your plan will be <b>automatically renewed</b> with its original
+      price without any discounts applied.
     </p>
   </div>
 );
